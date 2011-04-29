84e208ad0291d9c454d8414f9de589cc9ce30162
mawo78 <mawo78@gmail.com>
2011-04-29 20:18:06 +0200

    Now Android 2.1 compatible

--- src/cxa/lineswallpaper/GLES20LinesRenderer.java
+++ src/cxa/lineswallpaper/GLES20LinesRenderer.java
@@ -3,20 +3,27 @@ package cxa.lineswallpaper;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.Random;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+
+import com.badlogic.gdx.backends.android.AndroidGL20;
+import com.badlogic.gdx.graphics.GL20;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.opengl.GLES20;
+//import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.os.SystemClock;
 import android.util.Log;
 
 public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
+	
+	AndroidGL20 GLES20 = new AndroidGL20();
 
 	private static final int FLOAT_SIZE_BYTES = 4;
 
@@ -183,7 +190,7 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 
 	private void checkGlError(String op) {
 		int error;
-		while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+		while ((error = GLES20.glGetError()) != GL20.GL_NO_ERROR) {
 			Log.e(TAG, op + ": glError " + error);
 			throw new RuntimeException(op + ": glError " + error);
 		}
@@ -191,29 +198,31 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 	private int createFrameBuffer(GL10 gl, int width, int height,
 			int targetTextureId) {
 		int framebuffer;
-		int[] framebuffers = new int[1];
-		GLES20.glGenFramebuffers(1, framebuffers, 0);
-		framebuffer = framebuffers[0];
-		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer);
+		ByteBuffer tmp = ByteBuffer.allocateDirect(4);
+		tmp.order(ByteOrder.nativeOrder());
+		IntBuffer fb = tmp.asIntBuffer();
+		GLES20.glGenFramebuffers(1, fb);
+		framebuffer = fb.get(0);
+		GLES20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, framebuffer);
 
-		GLES20.glFramebufferTexture2D(GLES20.GL_FRAMEBUFFER,
-				GLES20.GL_COLOR_ATTACHMENT0, GL10.GL_TEXTURE_2D,
+		GLES20.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER,
+				GL20.GL_COLOR_ATTACHMENT0, GL10.GL_TEXTURE_2D,
 				targetTextureId, 0);
-		int status = GLES20.glCheckFramebufferStatus(GLES20.GL_FRAMEBUFFER);
-		if (status != GLES20.GL_FRAMEBUFFER_COMPLETE) {
+		int status = GLES20.glCheckFramebufferStatus(GL20.GL_FRAMEBUFFER);
+		if (status != GL20.GL_FRAMEBUFFER_COMPLETE) {
 			throw new RuntimeException("Framebuffer is not complete: "
 					+ Integer.toHexString(status));
 		}
-		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+		GLES20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, 0);
 		return framebuffer;
 	}
 	private int createProgram(String vertexSource, String fragmentSource) {
-		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+		int vertexShader = loadShader(GL20.GL_VERTEX_SHADER, vertexSource);
 		if (vertexShader == 0) {
 			return 0;
 		}
 
-		int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
+		int pixelShader = loadShader(GL20.GL_FRAGMENT_SHADER, fragmentSource);
 		if (pixelShader == 0) {
 			return 0;
 		}
@@ -225,9 +234,12 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 			GLES20.glAttachShader(program, pixelShader);
 			checkGlError("glAttachShader");
 			GLES20.glLinkProgram(program);
-			int[] linkStatus = new int[1];
-			GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
-			if (linkStatus[0] != GLES20.GL_TRUE) {
+			//int[] linkStatus = new int[1];
+			ByteBuffer tmp = ByteBuffer.allocateDirect(4);
+			tmp.order(ByteOrder.nativeOrder());
+			IntBuffer linkStatus = tmp.asIntBuffer();
+			GLES20.glGetProgramiv(program, GL20.GL_LINK_STATUS, linkStatus);
+			if (linkStatus.get(0) != GL20.GL_TRUE) {
 				Log.e(TAG, "Could not link program: ");
 				Log.e(TAG, GLES20.glGetProgramInfoLog(program));
 				GLES20.glDeleteProgram(program);
@@ -238,9 +250,12 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 	}
 	private int createTargetTexture(GL10 gl, int width, int height) {
 		int texture;
-		int[] textures = new int[1];
-		GLES20.glGenTextures(1, textures, 0);
-		texture = textures[0];
+		//int[] textures = new int[1];
+		ByteBuffer tmp = ByteBuffer.allocateDirect(4);
+		tmp.order(ByteOrder.nativeOrder());
+		IntBuffer textures = tmp.asIntBuffer();
+		GLES20.glGenTextures(1, textures);
+		texture = textures.get(0);
 		updateTargetTexture(gl, texture, width, height);
 		return texture;
 	}
@@ -252,12 +267,17 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 	}
 	private int loadShader(int shaderType, String source) {
 		int shader = GLES20.glCreateShader(shaderType);
+		Log.w(TAG, "loadShader returned " + Integer.valueOf(shader));
 		if (shader != 0) {
 			GLES20.glShaderSource(shader, source);
 			GLES20.glCompileShader(shader);
-			int[] compiled = new int[1];
-			GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-			if (compiled[0] == 0) {
+			ByteBuffer tmp = ByteBuffer.allocateDirect(4);
+			tmp.order(ByteOrder.nativeOrder());
+			IntBuffer compiled = tmp.asIntBuffer();
+			Log.w(TAG, "Before glGetShaderiv");
+			GLES20.glGetShaderiv(shader, GL20.GL_COMPILE_STATUS, compiled);
+			Log.w(TAG, "glGetShaderiv ret=" + compiled.get(0));
+			if (compiled.get(0) == 0) {
 				Log.e(TAG, "Could not compile shader " + shaderType + ":");
 				Log.e(TAG, GLES20.glGetShaderInfoLog(shader));
 				GLES20.glDeleteShader(shader);
@@ -272,17 +292,17 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 		// class's static methods instead.
 		GLES20.glClearColor(backgroundColorRed_, backgroundColorGreen_, backgroundColorBlue_, 1.0f);
 
-		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, framebuffer_);
+		GLES20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, framebuffer_);
 		GLES20.glViewport(0, 0, framebuffer_width_, framebuffer_height_);
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		GLES20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		renderBlurTexture();
 		renderLines();
 		
-		GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+		GLES20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, 0);
 		GLES20.glViewport(0, 0, surface_width_, surface_height_);
 
-		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		GLES20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		renderBlurTexture();
 	}
@@ -333,44 +353,47 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 		setupLinesShader();
 		setupFramebuffer(gl);
 
-		Matrix.setLookAtM(V_matrix_, 0, 0, 0, 1.0f, 0f, 0f, 0f, 0f, -1.0f, 0.0f);
+		setLookAtM( V_matrix_, 0, 0, 0, 1.0f, 0f, 0f, 0f, 0f, -1.0f, 0.0f);
 		Matrix.orthoM(quad_matrix_, 0, 0, 1, 0, 1, -1, 1);
 	}
 	private void renderBlurTexture() {
 		GLES20.glUseProgram(program_);
 		checkGlError("glUseProgram");
 
-		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, target_texture_);
+		GLES20.glActiveTexture(GL20.GL_TEXTURE0);
+		GLES20.glBindTexture(GL20.GL_TEXTURE_2D, target_texture_);
 		GLES20.glUniform1i(texture_loc_,0);
 
 		triangle_vertices_.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
-		GLES20.glVertexAttribPointer(position_handle_, 3, GLES20.GL_FLOAT,
+		GLES20.glVertexAttribPointer(position_handle_, 3, GL20.GL_FLOAT,
 				false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangle_vertices_);
 		checkGlError("glVertexAttribPointer maPosition");
 		triangle_vertices_.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
 		GLES20.glEnableVertexAttribArray(position_handle_);
 		checkGlError("glEnableVertexAttribArray position_handle_");
-		GLES20.glVertexAttribPointer(texture_handle_, 2, GLES20.GL_FLOAT,
+		GLES20.glVertexAttribPointer(texture_handle_, 2, GL20.GL_FLOAT,
 				false, TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangle_vertices_);
 		checkGlError("glVertexAttribPointer texture_handle_");
 		GLES20.glEnableVertexAttribArray(texture_handle_);
 		checkGlError("glEnableVertexAttribArray texture_handle_");
 
-		GLES20.glUniformMatrix4fv(MVP_matrix_handle_, 1, false, quad_matrix_, 0);
-		GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+		FloatBuffer fb = ByteBuffer.allocateDirect(quad_matrix_.length * FLOAT_SIZE_BYTES)
+			.order(ByteOrder.nativeOrder()).asFloatBuffer();
+		fb.put(quad_matrix_).position(0);
+		GLES20.glUniformMatrix4fv(MVP_matrix_handle_, 1, false, fb);
+		GLES20.glDrawArrays(GL20.GL_TRIANGLE_STRIP, 0, 4);
 		checkGlError("glDrawArrays");
 	}
 	private void renderLines() {
-		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
-		GLES20.glEnable(GLES20.GL_BLEND);
-		GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE);
+		GLES20.glBindTexture(GL20.GL_TEXTURE_2D, 0);
+		GLES20.glEnable(GL20.GL_BLEND);
+		GLES20.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
 
 		GLES20.glUseProgram(line_program_);
 		checkGlError("glUseProgram");
 
 		line_vertices_.position(0);
-		GLES20.glVertexAttribPointer(line_position_handle_, 3, GLES20.GL_FLOAT,
+		GLES20.glVertexAttribPointer(line_position_handle_, 3, GL20.GL_FLOAT,
 				false, FLOAT_SIZE_BYTES * 3, line_vertices_);
 		checkGlError("glVertexAttribPointer maPosition");
 		GLES20.glEnableVertexAttribArray(line_position_handle_);
@@ -385,9 +408,11 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 
 		GLES20.glUniform1f(line_delta_handle_, delta);
 		GLES20.glUniform3f(line_mColor_handle_, linesColorRed_, linesColorGreen_, linesColorBlue_);
-		GLES20.glUniformMatrix4fv(line_MVP_matrix_handle_, 1, false,
-				MVP_matrix_, 0);
-		GLES20.glDrawArrays(GLES20.GL_LINES, 0, LINE_COUNT);
+		FloatBuffer fb = ByteBuffer.allocateDirect(MVP_matrix_.length * FLOAT_SIZE_BYTES)
+			.order(ByteOrder.nativeOrder()).asFloatBuffer();
+		fb.put(MVP_matrix_).position(0);
+		GLES20.glUniformMatrix4fv(line_MVP_matrix_handle_, 1, false, fb);
+		GLES20.glDrawArrays(GL20.GL_LINES, 0, LINE_COUNT);
 		checkGlError("glDrawArrays lines");
 	}
 	private void setupFramebuffer(GL10 gl) {
@@ -476,12 +501,85 @@ public class GLES20LinesRenderer implements GLSurfaceView.Renderer {
 	}
 
 	private void updateTargetTexture(GL10 gl, int texture, int width, int height) {
-		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture);
-		GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGB, width,
-				height, 0, GLES20.GL_RGB, GLES20.GL_UNSIGNED_BYTE, null);
-		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-				GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-				GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+		GLES20.glBindTexture(GL20.GL_TEXTURE_2D, texture);
+		GLES20.glTexImage2D(GL20.GL_TEXTURE_2D, 0, GL20.GL_RGB, width,
+				height, 0, GL20.GL_RGB, GL20.GL_UNSIGNED_BYTE, null);
+		GLES20.glTexParameterf(GL20.GL_TEXTURE_2D,
+				GL20.GL_TEXTURE_MIN_FILTER, GL20.GL_LINEAR);
+		GLES20.glTexParameterf(GL20.GL_TEXTURE_2D,
+				GL20.GL_TEXTURE_MAG_FILTER, GL20.GL_LINEAR);
 	}
+	
+	/**
+     * Define a viewing transformation in terms of an eye point, a center of
+     * view, and an up vector.
+     *
+     * @param rm returns the result
+     * @param rmOffset index into rm where the result matrix starts
+     * @param eyeX eye point X
+     * @param eyeY eye point Y
+     * @param eyeZ eye point Z
+     * @param centerX center of view X
+     * @param centerY center of view Y
+     * @param centerZ center of view Z
+     * @param upX up vector X
+     * @param upY up vector Y
+     * @param upZ up vector Z
+     */
+    public static void setLookAtM(float[] rm, int rmOffset,
+            float eyeX, float eyeY, float eyeZ,
+            float centerX, float centerY, float centerZ, float upX, float upY,
+            float upZ) {
+
+        // See the OpenGL GLUT documentation for gluLookAt for a description
+        // of the algorithm. We implement it in a straightforward way:
+
+        float fx = centerX - eyeX;
+        float fy = centerY - eyeY;
+        float fz = centerZ - eyeZ;
+
+        // Normalize f
+        float rlf = 1.0f / Matrix.length(fx, fy, fz);
+        fx *= rlf;
+        fy *= rlf;
+        fz *= rlf;
+
+        // compute s = f x up (x means "cross product")
+        float sx = fy * upZ - fz * upY;
+        float sy = fz * upX - fx * upZ;
+        float sz = fx * upY - fy * upX;
+
+        // and normalize s
+        float rls = 1.0f / Matrix.length(sx, sy, sz);
+        sx *= rls;
+        sy *= rls;
+        sz *= rls;
+
+        // compute u = s x f
+        float ux = sy * fz - sz * fy;
+        float uy = sz * fx - sx * fz;
+        float uz = sx * fy - sy * fx;
+
+        rm[rmOffset + 0] = sx;
+        rm[rmOffset + 1] = ux;
+        rm[rmOffset + 2] = -fx;
+        rm[rmOffset + 3] = 0.0f;
+
+        rm[rmOffset + 4] = sy;
+        rm[rmOffset + 5] = uy;
+        rm[rmOffset + 6] = -fy;
+        rm[rmOffset + 7] = 0.0f;
+
+        rm[rmOffset + 8] = sz;
+        rm[rmOffset + 9] = uz;
+        rm[rmOffset + 10] = -fz;
+        rm[rmOffset + 11] = 0.0f;
+
+        rm[rmOffset + 12] = 0.0f;
+        rm[rmOffset + 13] = 0.0f;
+        rm[rmOffset + 14] = 0.0f;
+        rm[rmOffset + 15] = 1.0f;
+
+        Matrix.translateM(rm, rmOffset, -eyeX, -eyeY, -eyeZ);
+    }
 }
